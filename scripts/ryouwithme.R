e3a7b643caca5ee06ccd1060e7cd7829dbeb6f2c
Jasper <jasper.gp.robbins@gmail.com>
2023-02-25 21:25:17 +1100

    clean up 2

--- scripts/ryouwithme.R
+++ scripts/ryouwithme.R
@@ -7,6 +7,35 @@ library(janitor)
 
 beaches <- read_csv(here("data","sydneybeaches.csv"))
 
+# Shortcuts ####
+
+## shortcuts
+
+# Alt up/down      = move line up/down
+# Alt shift up     = duplicate line
+# Alt -            = <- 
+# Ctrl shift M     = %>%
+
+
+# Ctrl shift alt M = change all occurrences
+# Ctrl alt X       = wrap code in function
+# Ctrl alt V       = wrap as variable assignment
+
+# Ctrl Shift R  = foldable comment section
+# Alt L         = collapse active section
+# Alt O         = collapse all sections
+# Alt shift L   = open active section
+# Alt shift O   = open all sections
+
+## code snippets
+
+# type in and then press shift+tab to autocomplete
+# can add custom
+
+# lib, req, fun, ret, mat, for, while, switch, if, el, ei, apply, shinyapp
+
+# Data Loading ####
+
 # bring up view
 View(beaches)
 # dimensions
@@ -46,11 +75,49 @@ select(clean_beaches, council, site, beachbugs, everything())
 
 cleanbeaches <- beaches %>% 
   clean_names() %>%
-  rename(beachbugs = enterococci_cfu_100ml) %>%
-  select(council, site, beachbugs)
+  rename(beachbugs = enterococci_cfu_100ml)
 
 write_csv(clean_beaches, "cleanbeaches.csv")
-# https://rladiessydney.org/courses/ryouwithme/02-cleanitup-2/
-# https://learningstatisticswithr.com/lsr-0.6.pdf
+
+# which beach has most extreme levels of bugs
+
+# arrange (--> sort
+# desc(beachbugs) OR (-beachbugs)
+worstbugs <- clean_beaches %>% arrange(desc(beachbugs))
+
+# filter by value
+cleanbeaches %>% 
+  filter(site == "Coogee Beach") %>%
+  arrange(-beachbugs)
+
+# compare max bug values across different beaches
+# c() creates a vector
+cleanbeaches %>%
+  filter(site %in% c("Coogee Beach", "Bondi Beach")) %>%
+  arrange(-beachbugs)
+
+# summarise & group by
+# summarise doesn't work with missing data - need to explicitly say na.rm = TRUE
+# group_by gives us the values for the two separate sites rather than total
+cleanbeaches %>%
+  group_by(site) %>%
+  summarise(maxbug = max(beachbugs, na.rm = TRUE),
+            meanbugs = mean(beachbugs, na.rm = TRUE),
+            median = median(beachbugs, na.rm = TRUE),
+            sdbugs = sd(beachbugs, na.rm = TRUE))
+
+# compare councils
+cleanbeaches %>% distinct(council)
+
+councilbysite <- cleanbeaches %>% 
+  group_by(council, site) %>% 
+  summarise(meanbugs = mean(beachbugs, na.rm = TRUE),
+            median = median(beachbugs, na.rm = TRUE))
+
+# Plotting ####
+
+# https://rladiessydney.org/courses/ryouwithme/02-cleanitup-3/
+
+
 
 
